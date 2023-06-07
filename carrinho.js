d535c8eaed9bb3365e9540daaa288403b1798033
Laura Pigosso <127753547+LauraPigosso@users.noreply.github.com>
2023-06-07 16:57:24 -0300

    Add files via upload

--- carrinho.js
+++ carrinho.js
@@ -1,33 +1,43 @@
 window.onload = function () {
+  // Capturando todos os elementos com a classe 'product'
   var products = document.querySelectorAll('.product');
-  products.forEach(function (product) {
-    var minusButton = product.querySelector('.menos');
-    var plusButton = product.querySelector('.mais');
-    var quantitySpan = product.querySelector('.quantidade');
-    var quantity = 0;
 
+ 
+  products.forEach(function (product) {
+    var minusButton = product.querySelector('.menos'); // botao de diminuir quantidade
+    var plusButton = product.querySelector('.mais'); // botao de aumentar quantidade
+    var quantitySpan = product.querySelector('.quantidade'); // Capturando o elemento que exibe a quantidade
+    var quantity = 0; // Inicializando a quantidade como zero
+
+    // botao de diminuir quantidade
     minusButton.addEventListener('click', function () {
-      quantity = Math.max(0, quantity - 1);
-      quantitySpan.textContent = quantity;
-      updateTotal();
+      quantity = Math.max(0, quantity - 1); // Reduzindo a quantidade em 1, com valor minimo de zero
+      quantitySpan.textContent = quantity; // Atualizando o texto exibido da quantidade
+      updateTotal(); // Chamando a funcao para atualizar o valor total
     });
 
+    //clique do botao de aumentar quantidade
     plusButton.addEventListener('click', function () {
-      quantity = quantity + 1;
-      quantitySpan.textContent = quantity;
-      updateTotal();
+      quantity = quantity + 1; // Aumentando a quantidade em 1
+      quantitySpan.textContent = quantity; // Atualizando o texto exibido da quantidade
+      updateTotal(); // Chamando a funcao para atualizar o valor total
     });
   });
 
+  // atualizar o valor total
   function updateTotal() {
-    var totalQuantity = 0;
-    var totalValue = 0;
+    var totalQuantity = 0; // quantidade total como zero
+    var totalValue = 0; // valor total como zero
+
+   
     products.forEach(function (product) {
-      var quantity = parseInt(product.querySelector('.quantidade').textContent);
-      var price = parseFloat(product.querySelector('.product-price').textContent.replace('$', ''));
-      totalQuantity += quantity;
-      totalValue += quantity * price;
+      var quantity = parseInt(product.querySelector('.quantidade').textContent); // Obtendo a quantidade do produto
+      var price = parseFloat(product.querySelector('.product-price').textContent.replace('$', '')); // Obtendo o preco do produto
+      totalQuantity += quantity; // Somando a quantidade do produto pela quantidade total
+      totalValue += quantity * price; // Somando o preco do produto ao valor total
     });
+
+    // Atualizando os elementos que exibem a quantidade total e o valor total
     document.querySelector('#quantidade-total').textContent = totalQuantity;
     document.querySelector('#valor-total').textContent = '$' + totalValue.toFixed(2);
   }
